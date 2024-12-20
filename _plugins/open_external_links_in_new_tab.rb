7f5caac0e8d75ccc4dc67658e6d3d915678878c8
Tomas-Ka <tomas.kamsater@gmail.com>
2024-12-20 04:30:03 +0100

    Updated theme to be blue
    All of this is code stolen from another project of mine that used this
    template as as base.
    Since I'm using the template again, I figured I'd make sure to update
    the template as well whilst I was at it!

--- _plugins/open_external_links_in_new_tab.rb
+++ _plugins/open_external_links_in_new_tab.rb
@@ -20,8 +20,12 @@ def convert_links(doc)
 
   if open_external_links_in_new_tab
     parsed_doc = Nokogiri::HTML(doc.content)
-    parsed_doc.css("a:not(.internal-link):not(.footnote-backref)").each do |link|
-      link.set_attribute('target', 'blank')
+    parsed_doc.css("a:not(.footnote-backref):not(.internal-link)").each do |link|
+      link.set_attribute('target', '_blank')
+    end
+
+    parsed_doc.css("sup.footnote-ref a").each do |link|
+      link.remove_attribute('target')
     end
     doc.content = parsed_doc.to_html
   end
